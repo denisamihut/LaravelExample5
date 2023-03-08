3cff352fdf1e5de36d4b008bc6e4b9098d211b76
denisamihut <denisa.m_19@yahoo.com>
2023-03-08 17:27:48 +0200

    made functional form edit that ends at 1:10:00

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -55,7 +55,11 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $category->update([
+            'name' => $request->input('name')
+        ]);
+
+        return redirect()->route('categories.index');
     }
 
     /**
--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -11,13 +11,19 @@
                 <div class="p-6 text-gray-900">
                     <form method="POST" action="{{ route('categories.update', $category)}}">
                         @method('PUT')
+                        @csrf
                         Name:
                         <br/>
-                        <input name="name" value="{{ $category->name }}"/>
+                        <input type="text" name="name" value="{{ $category->name }}"
+                               class="block font-medium text-sm text-gray-700"/>
                         <br/><br/>
-                        <button type="submit">Save</button>
+                        <button type="submit"
+                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                            Save
+                        </button>
                     </form>
                 </div>
             </div>
         </div>
-    </div></x-app-layout>
+    </div>
+</x-app-layout>
