b605d7f0b2452c8b205f10bb315baea3e697422e
denisamihut <denisa.m_19@yahoo.com>
2023-03-08 16:54:24 +0200

    created CategoryController of 1:00:00 min

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+Route::resource('categories', \App\Http\Controllers\CategoryController::class);
