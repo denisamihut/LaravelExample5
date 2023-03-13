51daa5345dd501bab8160ceb8c3ccd42fb7cf022
denisamihut <denisa.m_19@yahoo.com>
2023-03-13 07:10:02 +0200

    made create new category functionality

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -23,7 +23,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('categories.create');
     }
 
     /**
@@ -31,7 +31,11 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Category::create([
+            'name' => $request->input('name')
+        ]);
+
+        return redirect()->route('categories.index');
     }
 
     /**
